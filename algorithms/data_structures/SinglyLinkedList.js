50583be66b33614f1eb8e2518a0e8452293d61f9
Neil M <neilm813@gmail.com>
2021-04-06 09:07:30 -0700

    algo requirements

--- algorithms/data_structures/SinglyLinkedList.js
+++ algorithms/data_structures/SinglyLinkedList.js
@@ -93,47 +93,84 @@ class SinglyLinkedList {
     }
     return this;
   }
+
+  /**
+   * Converts this list into an array containing the data of each node.
+   * - Time: O(n) linear.
+   * - Space: O(n).
+   * @returns {Array<any>} An array of each node's data.
+   */
+  toArr() {
+    const arr = [];
+    let runner = this.head;
+
+    while (runner) {
+      arr.push(runner.data);
+      runner = runner.next;
+    }
+    return arr;
+  }
+
+  /**
+   * Creates a new node with the given data and inserts that node at the front
+   * of this list.
+   * - Time: (?).
+   * - Space: (?).
+   * @param {any} data The data for the new node.
+   * @returns {SinglyLinkedList} This list.
+   */
+  insertAtFront(data) {}
+
+  /**
+   * Removes the first node of this list.
+   * - Time: (?).
+   * - Space: (?).
+   * @returns {any} The data from the removed node.
+   */
+  removeHead() {}
+
+  // EXTRA
+  /**
+   * Calculates the average of this list.
+   * - Time: (?).
+   * - Space: (?).
+   * @returns {number|NaN} The average of the node's data.
+   */
+  average() {}
 }
 
 const emptyList = new SinglyLinkedList();
-
-/***************** Uncomment after insertAtBack method is created. ****************/
 const singleNodeList = new SinglyLinkedList().insertAtBack(1);
 const biNodeList = new SinglyLinkedList().insertAtBack(1).insertAtBack(2);
-
-console.log(singleNodeList);
-console.log(biNodeList);
-
-/***************** Uncomment after seedFromArr method is created. ****************/
-// const firstThreeList = new SinglyLinkedList().seedFromArr([1, 2, 3]);
-// const secondThreeList = new SinglyLinkedList().seedFromArr([4, 5, 6]);
-// const unorderedList = new SinglyLinkedList().seedFromArr([
-//   -5,
-//   -10,
-//   4,
-//   -3,
-//   6,
-//   1,
-//   -7,
-//   -2,
-// ]);
+const firstThreeList = new SinglyLinkedList().seedFromArr([1, 2, 3]);
+const secondThreeList = new SinglyLinkedList().seedFromArr([4, 5, 6]);
+const unorderedList = new SinglyLinkedList().seedFromArr([
+  -5,
+  -10,
+  4,
+  -3,
+  6,
+  1,
+  -7,
+  -2,
+]);
 
 // // node 4 connects to node 1, back to head
-// const perfectLoopList = new SinglyLinkedList().seedFromArr([1, 2, 3, 4]);
-// perfectLoopList.head.next.next.next = perfectLoopList.head;
+const perfectLoopList = new SinglyLinkedList().seedFromArr([1, 2, 3, 4]);
+perfectLoopList.head.next.next.next = perfectLoopList.head;
 
 // // node 4 connects to node 2
-// const loopList = new SinglyLinkedList().seedFromArr([1, 2, 3, 4]);
-// loopList.head.next.next.next = loopList.head.next;
+const loopList = new SinglyLinkedList().seedFromArr([1, 2, 3, 4]);
+loopList.head.next.next.next = loopList.head.next;
 
-// const sortedDupeList = new SinglyLinkedList().seedFromArr([
-//   1,
-//   1,
-//   1,
-//   2,
-//   3,
-//   3,
-//   4,
-//   5,
-//   5,
-// ]);
+const sortedDupeList = new SinglyLinkedList().seedFromArr([
+  1,
+  1,
+  1,
+  2,
+  3,
+  3,
+  4,
+  5,
+  5,
+]);
