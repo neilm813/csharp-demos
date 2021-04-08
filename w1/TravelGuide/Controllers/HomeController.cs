f27f109abc8c56049e022459aba943e70eb84b67
Neil M <neilm813@gmail.com>
2021-04-08 11:29:05 -0700

    travel guide

---
+++ w1/TravelGuide/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TravelGuide.Controllers
+{
+    public class HomeController : Controller // <- inherit from the base abstract Controller
+    {
+        [HttpGet("")] // attribute, pattern is: [HttpVerb("url")]
+        public ViewResult Index()
+        {
+            ViewBag.Introduction = "My name is Falconhoof and I will be your guide on your quest.";
+            ViewBag.YearCreated = "2021";
+            ViewBag.ImgUrl = "https://ih1.redbubble.net/image.230236822.2041/raf,750x1000,075,t,101010:01c5ca27c6.u3.jpg";
+
+            /* 
+            If no string passed in, will use look for a .cshtml file named the
+            same as this method (action).
+            */
+            return View("Index");
+        }
+
+        [HttpGet("travel/{destination}")]
+        public RedirectToActionResult Unknown(string destination)
+        {
+            Console.WriteLine($"URL destination: {destination}");
+
+            // redirect to Index method (action).
+            return RedirectToAction("Index");
+        }
+    }
+}
