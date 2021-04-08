c83d454d014f059f5f12dff99a7179aaa0076508
Neil M <neilm813@gmail.com>
2021-04-08 08:53:02 -0700

    algo reqs

--- algorithms/data_structures/SinglyLinkedList.js
+++ algorithms/data_structures/SinglyLinkedList.js
@@ -264,6 +264,36 @@ class SinglyLinkedList {
     }
     return removedData;
   }
+
+  /**
+   * Retrieves the data of the second to last node in this list.
+   * - Time: (?).
+   * - Space: (?).
+   * @returns {any} The data of the second to last node or null if there is no
+   *    second to last node.
+   */
+  secondToLast() {}
+
+  /**
+   * Removes the node that has the matching given val as it's data.
+   * - Time: (?).
+   * - Space: (?).
+   * @param {any} val The value to compare to the node's data to find the
+   *    node to be removed.
+   * @returns {boolean} Indicates if a node was removed or not.
+   */
+  removeVal(val) {}
+
+  // EXTRA
+  /**
+   * Inserts a new node before a node that has the given value as its data.
+   * - Time: (?).
+   * - Space: (?).
+   * @param {any} newVal The value to use for the new node that is being added.
+   * @param {any} targetVal The value to use to find the node that the newVal
+   *    should be inserted in front of.
+   */
+  prepend(newVal, targetVal) {}
 }
 
 const emptyList = new SinglyLinkedList();
