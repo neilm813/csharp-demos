d73376a73085bd05a1a59366bf827c32043c93e6
Neil M <neilm813@gmail.com>
2021-04-09 11:35:22 -0700

    hormuz image url

--- w1/TravelGuide/Models/TravelDestinations.cs
+++ w1/TravelGuide/Models/TravelDestinations.cs
@@ -10,7 +10,7 @@ namespace TravelGuide.Models
             new Destination("Solovetsky Islands", "https://i.dailymail.co.uk/i/pix/2013/11/04/article-2487290-192FC96800000578-337_964x610.jpg"),
             new Destination("Socotra", "https://i.redd.it/fzpdoqe3zgn41.jpg"),
             new Destination("Bhutan", "https://www.adventurewomen.com/wp-content/uploads/2018/04/1.-Tigers-Nest-800x533.jpg"),
-            new Destination("Hormuz Island", "https://i.pinimg.com/736x/20/17/d7/2017d72bcdc123c96806baa2c60cd13d.jpg"),
+            new Destination("Hormuz Island", "https://i.pinimg.com/originals/7b/5d/fe/7b5dfe78e7a86eb2808c200973a46a83.jpg"),
             new Destination("Giant Crystal Cave", "https://cdn.tourismontheedge.com/wp-content/uploads/2013/01/Naica.Mine_.jpg"),
             new Destination("Hell", "https://i.guim.co.uk/img/static/sys-images/Guardian/Pix/pictures/2014/7/17/1405616925070/e1c101a3-2d79-4c22-869b-d65473479e5d-2060x1373.jpeg?width=940&quality=85&auto=format&fit=max&s=ba7e6537b8b4eba098f852088fe7c27d"),
             new Destination("Bed", "https://img-9gag-fun.9cache.com/photo/arovENB_460s.jpg")
