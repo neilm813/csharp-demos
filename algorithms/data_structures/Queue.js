14c5277ba90d8def19d83c69daf1af463fec0c9d
Neil M <neilm813@gmail.com>
2021-04-14 10:08:00 -0700

    algos

--- algorithms/data_structures/Queue.js
+++ algorithms/data_structures/Queue.js
@@ -87,8 +87,29 @@ class Queue {
    * @returns {boolean} Whether all the items of the two queues are equal and
    *    in the same order.
    */
+  compareQueues(q2) {
+    if (this.size() !== q2.size()) {
+      return false;
+    }
+    let count = 0;
+    let isEqual = true;
+    const len = this.size();
 
-  compareQueues(q2) {}
+    while (count < len) {
+      const dequeued1 = this.dequeue();
+      const dequeued2 = q2.dequeue();
+
+      if (dequeued1 !== dequeued2) {
+        isEqual = false;
+      }
+
+      this.enqueue(dequeued1);
+      q2.enqueue(dequeued2);
+      count++;
+    }
+    return isEqual;
+  }
+
   /**
    * Determines if the queue is a palindrome (same items forward and backwards).
    * Avoid indexing queue items directly via bracket notation, instead use the
@@ -101,6 +122,28 @@ class Queue {
    * @returns {boolean}
    */
   isPalindrome() {
-    const stack = new Stack();
+    let isPalin = true;
+    const stack = new Stack(),
+      len = this.size();
+
+    for (let i = 0; i < len; i++) {
+      let dequeued = this.dequeue();
+      stack.push(dequeued);
+      // add it back so the queue items and order is restored at the end
+      this.enqueue(dequeued);
+    }
+
+    for (let i = 0; i < len; i++) {
+      let dequeued = this.dequeue();
+      let popped = stack.pop();
+
+      if (popped !== dequeued) {
+        isPalin = false;
+      }
+
+      // add it back so the queue items and order is restored at the end
+      this.enqueue(dequeued);
+    }
+    return isPalin;
   }
 }
