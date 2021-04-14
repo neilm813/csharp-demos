3a49fbb791adbc0f2a3a395b34385e5c93ba28e3
Neil M <neilm813@gmail.com>
2021-04-14 09:03:33 -0700

    algos

--- algorithms/data_structures/Queue.js
+++ algorithms/data_structures/Queue.js
@@ -71,4 +71,32 @@ class Queue {
   size() {
     return this.items.length;
   }
+
+  /**
+   * Compares this queue to another given queue to see if they are equal.
+   * Avoid indexing the queue items directly via bracket notation, use the
+   * queue methods instead for practice.
+   * Use no extra array or objects.
+   * The queues should be returned to their original order when done.
+   * - Time: O(n^2) quadratic, n = queue length. Quadratic due to dequeue on an
+   *     array queue being O(n).
+   * - Space: O(1) constant.
+   * @param {Queue} q2 The queue to be compared against this queue.
+   * @returns {boolean} Whether all the items of the two queues are equal and
+   *    in the same order.
+   */
+
+  compareQueues(q2) {}
+  /**
+   * Determines if the queue is a palindrome (same items forward and backwards).
+   * Avoid indexing queue items directly via bracket notation, instead use the
+   * queue methods for practice.
+   * Use only 1 stack as additional storage, no other arrays or objects.
+   * The queue should be returned to its original order when done.
+   * - Time: O(n^2) quadratic, n = queue length. Quadratic due to dequeue on an
+   *     array queue being O(n).
+   * - Space: O(n) from the stack being used to store the items again.
+   * @returns {boolean}
+   */
+  isPalindrome() {}
 }
