24fcaba57df2114dc689a48f962368e28049ac70
Neil M <neilm813@gmail.com>
2021-06-23 08:58:15 -0700

    algo reqs

--- algorithms/data_structures/BinarySearchTree.js
+++ algorithms/data_structures/BinarySearchTree.js
@@ -199,6 +199,28 @@ class BinarySearchTree {
     }
     return this.max(startNode) - this.min(startNode);
   }
+
+  /**
+   * Inserts a new node with the given newVal in the right place to preserver
+   * the order of this tree.
+   * - Time: O(?).
+   * - Space: O(?).
+   * @param {number} newVal The data to be added to a new node.
+   * @returns {BinarySearchTree} This tree.
+   */
+  insert(newVal) {}
+
+  /**
+   * Inserts a new node with the given newVal in the right place to preserver
+   * the order of this tree.
+   * - Time: O(?).
+   * - Space: O(?).
+   * @param {number} newVal The data to be added to a new node.
+   * @param {Node} curr The node that is currently accessed from the tree as
+   *    the tree is being traversed.
+   * @returns {BinarySearchTree} This tree.
+   */
+  insertRecursive(newVal, curr = this.root) {}
 }
 
 const emptyTree = new BinarySearchTree();
