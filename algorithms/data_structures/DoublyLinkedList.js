dc63e63a29444f322fa8787faeb453498aefcded
Neil M <neilm813@gmail.com>
2021-06-30 09:05:15 -0700

    algo reqs

--- algorithms/data_structures/DoublyLinkedList.js
+++ algorithms/data_structures/DoublyLinkedList.js
@@ -116,6 +116,28 @@ class DoublyLinkedList {
     }
     return this;
   }
+
+  /**
+   * Inserts a new node with the given newVal after the node that has the
+   * given targetVal as it's data.
+   * - Time: O(?).
+   * - Space: O(?).
+   * @param {any} targetVal The node data to find.
+   * @param {any} newVal Data for the new node.
+   * @returns {boolean} Indicates if the new node was added.
+   */
+  insertAfter(targetVal, newVal) {}
+
+  /**
+   * Inserts a new node with the given newVal before the node that has the
+   * given targetVal as it's data.
+   * - Time: O(?).
+   * - Space: O(?).
+   * @param {any} targetVal The node data to find.
+   * @param {any} newVal Data for the new node.
+   * @returns {boolean} Indicates if the new node was added.
+   */
+  insertBefore(targetVal, newVal) {}
 }
 
 const emptyList = new DoublyLinkedList();
