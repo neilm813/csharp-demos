dd6cbf02ea7ae9eb5625ef8c65cc08984b771d1b
Neil M <neilm813@gmail.com>
2021-07-02 11:52:34 -0700

    algo slns

--- algorithms/data_structures/Heap.js
+++ algorithms/data_structures/Heap.js
@@ -18,32 +18,6 @@ class MinHeap {
   }
 
   /**
-   * Logs the tree horizontally with the root on the left and the index in
-   * parenthesis using reverse inorder traversal.
-   */
-  printHorizontalTree(parentIdx = 1, spaceCnt = 0, spaceIncr = 10) {
-    if (parentIdx > this.heap.length - 1) {
-      return;
-    }
-
-    spaceCnt += spaceIncr;
-    this.printHorizontalTree(parentIdx * 2 + 1, spaceCnt);
-
-    console.log(
-      " ".repeat(spaceCnt < spaceIncr ? 0 : spaceCnt - spaceIncr) +
-        `${this.heap[parentIdx]} (${parentIdx})`
-    );
-
-    this.printHorizontalTree(parentIdx * 2, spaceCnt);
-  }
-
-  printArr(...appendedMsgs) {
-    const arrStr = `\n[${["null", ...this.heap.slice(1)].join(", ")}]`;
-    const msgLen = arrStr.length + appendedMsgs.toString().length;
-    console.log("-".repeat(msgLen), arrStr, ...appendedMsgs);
-  }
-
-  /**
    * Retrieves the size of the heap, ignoring the null placeholder.
    * - Time: O(1) constant.
    * - Space: O(1) constant.
@@ -76,6 +50,7 @@ class MinHeap {
   insert(num) {
     this.heap.push(num);
     this.shiftUp();
+    // .push on array returns the new length
     return this.size();
   }
 
@@ -120,18 +95,152 @@ class MinHeap {
    * Extracts the min num from the heap and then re-orders the heap to
    * maintain order so the next min is ready to be extracted.
    * 1. Save the first node to a temp var.
-   * 2. Pop last node off and set idx1 equal to the popped value.
+   * 2. Pop last node off and overwrite idx1 with it.
    * 3. Iteratively swap the old last node that is now at idx1 with it's
    *    smallest child IF the smallest child is smaller than it.
    * - Time: O(log n) logarithmic due to shiftDown.
    * - Space: O(1) constant.
    * @returns {?number} The min number or null if empty.
    */
-  extract() {}
+  extract() {
+    // nothing to remove
+    if (this.heap.length === 1) {
+      return null;
+    }
+
+    const heap = this.heap,
+      min = heap[1],
+      lastNode = heap.pop();
+
+    // last item is being removed, no more work required
+    if (heap.length === 1) {
+      return min;
+    }
+
+    // last node is overwriting the idx 1 to "remove" idx 1
+    heap[1] = lastNode;
+    // since we put the lastNode at the start, it needs to be swapped down to it's correct position to restore the order
+    this.shiftDown();
+    return min;
+  }
+
+  // AKA: siftDown, heapifyDown, bubbleDown, sinkDown to restore order after extracting min
+  shiftDown() {
+    const heap = this.heap;
+
+    let idxOfNodeToShiftDown = 1,
+      idxOfLeftChild = idxOfNodeToShiftDown * 2;
+
+    this.printArr(
+      `shiftDown start from front to shift ${heap[idxOfNodeToShiftDown]} down:`
+    );
+    this.printHorizontalTree();
+
+    // while there is at least 1 child
+    while (this.idxOfLeftChild < heap.length) {
+      const idxOfRightChild = this.idxOfLeftChild + 1;
+      let idxOfSmallestChild = this.idxOfLeftChild;
+
+      if (
+        idxOfRightChild < heap.length &&
+        heap[idxOfRightChild] < heap[this.idxOfLeftChild]
+      ) {
+        idxOfSmallestChild = idxOfRightChild;
+      }
+
+      // no more swapping needed, no children are smaller
+      if (heap[idxOfNodeToShiftDown] <= heap[idxOfSmallestChild]) {
+        break;
+      }
+
+      // swap the smallest child with the parent since the parent is not smaller
+      [heap[idxOfNodeToShiftDown], heap[idxOfSmallestChild]] = [
+        heap[idxOfSmallestChild],
+        heap[idxOfNodeToShiftDown],
+      ];
+
+      this.printArr(
+        `shiftDown swapped ${heap[idxOfSmallestChild]} & ${heap[idxOfNodeToShiftDown]}:`
+      );
+      this.printHorizontalTree();
+
+      // follow this node since it was just swapped to see if it needs to be swapped again
+      idxOfNodeToShiftDown = idxOfSmallestChild;
+      idxOfLeftChild = idxOfNodeToShiftDown * 2;
+    }
+    console.log("shiftDown done\n");
+  }
+
+  // static allows this method to be executed directly on the class itself rather than needing to be executed on an instance or going through the .prototype
+  // converts an array into a new heap
+  static heapify(vals) {
+    const heap = new MinHeap();
+    vals.forEach((val) => heap.insert(val));
+    return heap;
+  }
+
+  // prints tree with root on left and index in parens in reverse inorder traversal
+  // https://www.geeksforgeeks.org/print-binary-tree-2-dimensions/
+  printHorizontalTree(parentIdx = 1, spaceCnt = 0, spaceIncr = 10) {
+    if (parentIdx > this.heap.length - 1) {
+      return;
+    }
+
+    spaceCnt += spaceIncr;
+    this.printHorizontalTree(parentIdx * 2 + 1, spaceCnt);
+
+    console.log(
+      " ".repeat(spaceCnt < spaceIncr ? 0 : spaceCnt - spaceIncr) +
+        `${this.heap[parentIdx]} (${parentIdx})`
+    );
+
+    this.printHorizontalTree(parentIdx * 2, spaceCnt);
+  }
+
+  printArr(...appendedMsgs) {
+    const arrStr = `\n[${["null", ...this.heap.slice(1)].join(", ")}]`;
+    const msgLen = arrStr.length + appendedMsgs.toString().length;
+    console.log("-".repeat(msgLen), arrStr, ...appendedMsgs);
+  }
+}
+
+// Time: O(n log n) linearithmic because .insert is O(log n) and .insert is being done n times
+// unstable
+function heapSort(nums) {
+  const minHeap = MinHeap.heapify(nums);
+  const sorted = [];
+
+  // if we had a max heap, we could loop backwards and instead of push assign by index
+  for (let i = 0; i < nums.length; i++) {
+    sorted.push(minHeap.extract());
+  }
+  return sorted;
+}
+
+function heapSortInPlace(nums) {
+  const minHeap = MinHeap.heapify(nums);
+
+  for (let i = 0; i < nums.length; i++) {
+    nums[i] = minHeap.extract();
+  }
+  return nums;
 }
 
 const minHeap = new MinHeap();
-minHeap.insert(10);
-minHeap.insert(20);
-minHeap.insert(15);
 minHeap.insert(5);
+minHeap.insert(4);
+minHeap.insert(7);
+minHeap.insert(3);
+minHeap.insert(6);
+minHeap.insert(2);
+//   minHeap.insert(1)
+//   minHeap.insert(0);
+// console.log(`extracted ${minHeap.extract()}`);
+
+minHeap.extract();
+minHeap.extract();
+minHeap.extract();
+minHeap.extract();
+minHeap.extract();
+minHeap.extract();
+module.exports = { MinHeap };
